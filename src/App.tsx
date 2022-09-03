c51c7b347d6314dea7e59bbcd39dfe325ef73148
AnnAntonova2310 <anyashaida@mail.ru>
2022-09-03 19:02:06 +0300

    Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,48 @@
 import React, {useState} from 'react';
 import './App.css';
-import NewComponent from "./NewComponent";
+import {FullInput} from "./components/FullInput";
+import {Input} from "./components/Input";
+import {Button} from "./components/Button";
 
-export type FilterType = 'ruble' | 'dollar' | 'all'
 
 function App() {
-    const [money, setMoney] = useState([
-        {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' z1234567890'},
-        {banknote: 'ruble', nominal: 100, number: ' w1234567890'},
-        {banknote: 'dollar', nominal: 100, number: ' e1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' c1234567890'},
-        {banknote: 'ruble', nominal: 100, number: ' r1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' x1234567890'},
-        {banknote: 'ruble', nominal: 50, number: ' v1234567890'},
-    ])
 
-    const [filter, setFilter] = useState<FilterType>('all')
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
 
-    let currentMoney = money
-    if (filter === 'ruble') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'ruble')
+        ]
+    )
+
+    let [title, setTitle] = useState('')
+
+    const addMessage = (title: string) => {
+        let newMessage = {message: title};
+        setMessage([newMessage, ...message])
+        setTitle('')
     }
-
-    if (filter === 'dollar') {
-        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === 'dollar')
-    }
-
-    const onClickFilterHandler = (nameButton: FilterType) => {
-        setFilter(nameButton)
-    }
-
 
     return (
-      <NewComponent
-          currentMoney={currentMoney}
-          onClickFilterHandler={onClickFilterHandler}
-      />
+        <div className={'App'}>
+            {/*<div>*/}
+            {/*    <input/>*/}
+            {/*    <button>+</button>*/}
+            {/*</div>*/}
 
-    );
+            {/*<FullInput addMessage={addMessage}/>*/}
+
+            <Input setTitle={setTitle} title={title}/>
+            <Button name={'+'} callback={() => addMessage(title)}/>
+
+
+            {message.map((el, index) => {
+                return (
+                    <div key={index}>{el.message}</div>
+                )
+            })}
+        </div>
+    )
 }
 
 export default App;
-
